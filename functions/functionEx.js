7ce24be5888838fb6d5e536b32b053906e3b8dc0
Paula Fiaschi <paul6399@stud.kea.dk>
2022-02-14 14:49:50 +0100

    capitalization

--- functions/functionEx.js
+++ functions/functionEx.js
@@ -1,7 +1,23 @@
-let firstName;
+// function sayHello(firstName) {
+//   console.log(`Hello ${firstName}`);
+// }
 
-function sayHello(firstName) {
-  console.log(`Hello ${firstName}`);
+// sayHello("Robert");
+
+// function presentPet(firstName, animalType, animalName) {
+//   console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}`);
+// }
+
+// presentPet("Robert", "dog", "Simba");
+// let str = "ignacio";
+
+function capitalize(str) {
+  let firtsLetter = str.charAt(0).toUpperCase();
+  let restOfName = str.substring(1).toLowerCase();
+  console.log(`${firtsLetter}${restOfName}`);
+  // console.log(str.charAt(0).toUpperCase());
+  // console.log(str.substring(1));
 }
 
-sayHello("Robert");
+let unNombre = "aiLin";
+capitalize(unNombre);
