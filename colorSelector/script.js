1a10cc7882436f4b01d62ce13bd087ebea5a8e31
Paula Fiaschi <paul6399@stud.kea.dk>
2022-02-16 14:30:03 +0100

    to hex

--- colorSelector/script.js
+++ colorSelector/script.js
@@ -20,28 +20,52 @@ function showBoxColored(colorPicked) {
 }
 
 function showHex(colorPicked) {
-  document.querySelector("#hexCode").innerHTML = "HEX: " + colorPicked;
+  document.querySelector("#hexCode").innerHTML = "HEX: " + colorPicked.toUpperCase();
   return convertToRgb(colorPicked);
 }
 
 function convertToRgb(colorpicked) {
   let hexString = colorpicked.toString();
-  console.log("hex to string: " + hexString);
 
   let rAsHex = hexString.substring(1, 3);
   let gAsHex = hexString.substring(3, 5);
   let bAsHex = hexString.substring(5, 7);
 
   let rAsInteger = parseInt(rAsHex, 16);
-  console.log("r to string: " + rAsInteger);
 
   let gAsInteger = parseInt(gAsHex, 16);
-  console.log("g to string: " + gAsInteger);
 
   let bAsInteger = parseInt(bAsHex, 16);
-  console.log("b to string: " + bAsInteger);
 
+  // console.log("to RGB: (" + rAsInteger + ", " + gAsInteger + ", " + bAsInteger + ")");
   document.querySelector("#rgbCode").innerHTML = "RGB: (" + rAsInteger + ", " + gAsInteger + ", " + bAsInteger + ")";
+
+  let rgbObj = { rAsInteger, gAsInteger, bAsInteger };
+  console.log(rgbObj);
+
+  // let cssColor = "background-color: rgb(" + rAsInteger + ", " + gAsInteger + ", " + bAsInteger + ")";
+  // console.log(cssColor);
+  return convertToHex(rgbObj);
+}
+
+function convertToHex(rgbObj) {
+  let rHex = rgbObj.rAsInteger.toString(16);
+  let gHex = rgbObj.gAsInteger.toString(16);
+  let bHex = rgbObj.bAsInteger.toString(16);
+
+  if (rHex.length == 1) {
+    rHex = "0" + rHex;
+  }
+  if (gHex.length == 1) {
+    gHex = "0" + gHex;
+  }
+  if (bHex.length == 1) {
+    bHex = "0" + bHex;
+  }
+
+  let hexValue = "#" + rHex + gHex + bHex;
+  console.log("to hex: " + hexValue);
+  return hexValue;
 }
 
 // //Convert from hex to integer
